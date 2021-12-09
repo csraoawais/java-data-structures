3ae78bd1182f993d4f06397154538f5e57f87ebe
csraoawais <csraoawais@gmail.com>
2021-12-09 14:13:41 +0500

    Loops

--- src/main/java/WorkingWithLists.java
+++ src/main/java/WorkingWithLists.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class WorkingWithLists {
     public static void main(String[] args) {
-        List<String> obj= new ArrayList<>();
+        List<String> obj = new ArrayList<>();
         obj.add("awais");
         obj.add("javed");
         System.out.println(obj);
@@ -11,6 +11,16 @@ public class WorkingWithLists {
         System.out.println(obj.contains("awais"));
         System.out.println(obj.contains("Ali"));
         System.out.println(obj);
+        //The way how to use loop with lists
+        for (String objects : obj) {
+            System.out.println(objects);
+        }
+        //foreach loop
+        obj.forEach(System.out::println);
+        //traditional for loop
+        for (int i = 0; i < obj.size(); i++) {
+            System.out.println(obj.get(i));
 
+        }
     }
 }
